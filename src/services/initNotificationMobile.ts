f117f96c6e3303b17e4b3ee80b55c5161d2a0201
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2020-08-13 14:37:11 +0700

    update notification web, mobile

---
+++ src/services/initNotificationMobile.ts
@@ -0,0 +1,94 @@
+const platformDevice = 'iOS';
+
+export const getPushTokenDevice = async (isLogin: boolean) => {
+  return new Promise((resolve, reject) => {
+    const FirebasePlugin = (window as any).FirebasePlugin;
+    const getFcmToken = localStorage.getItem('__fcmToken__');
+
+    const getToken = async () => {
+      const getLatestFcmToken = () => {
+        return new Promise((resolveNewFcmToken) => {
+          FirebasePlugin.onTokenRefresh(resolveNewFcmToken);
+        });
+      };
+
+      const unregisterFcmToken = () => {
+        return new Promise((resolveUnregister, rejectUnregister) => {
+          FirebasePlugin.unregister(resolveUnregister, rejectUnregister);
+        });
+      };
+
+      if (isLogin) {
+        // after login success, try to get new fcmToken by unregister (3 times)
+        // then return the new fcmToken
+        const tryTime = 3;
+        let countUp = 1;
+        let token: any = '';
+
+        while ((!token || token === getFcmToken) && countUp <= tryTime) {
+          await unregisterFcmToken();
+          token = await getLatestFcmToken();
+          countUp++;
+        }
+        if (token) {
+          resolve(token);
+        } else {
+          reject({
+            error: 'Cannot get new fcmToken',
+          });
+        }
+      } else {
+        // return the latest fcmToken
+        try {
+          const token = await getLatestFcmToken();
+          resolve(token);
+        } catch (error) {
+          reject(error);
+        }
+      }
+    };
+
+    switch (platformDevice) {
+      case 'Android':
+        getToken();
+        break;
+      case 'iOS':
+        FirebasePlugin.hasPermission((hasPermission: any) => {
+          if (hasPermission) {
+            getToken();
+          } else {
+            FirebasePlugin.grantPermission((grantPermission: any) => {
+              if (grantPermission) {
+                getToken();
+              } else {
+                reject({
+                  error: 'User does not allow permission',
+                });
+              }
+            });
+          }
+        }, reject);
+        break;
+    }
+  });
+};
+
+const saveFcmToken = async (fcmToken: string) => {
+  console.log(fcmToken);
+};
+
+const initNotificationMobile = async (isLogin: boolean) => {
+  const FirebasePlugin = (window as any).FirebasePlugin;
+
+  if (FirebasePlugin) {
+    getPushTokenDevice(isLogin)
+      .then((fcmToken: string) => {
+        saveFcmToken(fcmToken);
+      })
+      .catch((error) => {
+        console.log(`getPushTokenDevice error: ${error}`);
+      });
+  }
+};
+
+export default initNotificationMobile;
---
+++ src/services/initNotificationWeb.ts
@@ -0,0 +1,60 @@
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+
+export const config = {
+  appId: process.env.REACT_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  publicVAPIDkey: process.env.REACT_APP_PUBLIC_VAPID_KEY,
+};
+
+firebase.initializeApp(config);
+
+const initNotificationWeb = async () => {
+  const messaging = firebase.messaging();
+
+  messaging.usePublicVapidKey(
+    // Project Settings => Cloud Messaging => Web Push certificates
+    config.publicVAPIDkey || 'xxxx',
+  );
+
+  try {
+    await messaging.requestPermission();
+    const fcmToken: string | null = await messaging.getToken();
+    if (!fcmToken) return;
+  } catch (error) {
+    if (error.code === 'messaging/permission-blocked') {
+      console.log('FCM: Please Unblock Notification Request Manually');
+    } else {
+      console.log(`FCM: Error Occurred : ${error}`);
+    }
+  }
+};
+
+export const requestFirebaseNotificationPermission = () =>
+  new Promise((resolve, reject) => {
+    const messaging = firebase.messaging();
+    messaging
+      .requestPermission()
+      .then(() => messaging.getToken())
+      .then((firebaseToken: unknown) => {
+        resolve(firebaseToken);
+      })
+      .catch((err: any) => {
+        reject(err);
+      });
+  });
+
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    const messaging = firebase.messaging();
+    messaging.onMessage((payload: unknown) => {
+      resolve(payload);
+    });
+  });
+
+export default initNotificationWeb;
