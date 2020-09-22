c273ce616df00327cdbb42065c2146cc8e0a854d
truong.nguyen <truong.nguyen@persol.co.jp>
2020-09-22 08:45:16 +0700

    write test saga

--- src/features/Dashboard/redux/saga.test.ts
+++ src/features/Dashboard/redux/saga.test.ts
@@ -35,3 +35,36 @@ describe('DASHBOARD SAGA', () => {
     });
   });
 });
+
+// WRITE TEST WITH MOCK API
+// describe('DASHBOARD SAGA', () => {
+//   describe('FETCH TODO FLOW', () => {
+//     it('SHOULD YIELD FETCH REQUEST', () => {
+//       return expectSaga(dashboardSaga).take(IDashboardTypes.FETCH_TODO_REQUEST).run();
+//     });
+
+//     it('SHOULD YIELD FETCH SUCCESS', () => {
+//       jest.spyOn(window, 'fetch').mockImplementation(() => {
+//         const fetchResponse = {
+//           json: () => Promise.resolve({ data: undefined }),
+//         };
+//         return Promise.resolve(fetchResponse);
+//       });
+//       return expectSaga(fetchTodoSaga)
+//         .put({
+//           type: IDashboardTypes.FETCH_TODO_SUCCESS,
+//           payload: {
+//             todos: undefined,
+//           },
+//         })
+//         .run();
+//     });
+
+//     it('SHOULD YIELD FETCH ERROR', () => {
+//       jest.spyOn(window, 'fetch').mockImplementation(() => {
+//         return Promise.resolve({ ok: false, json: () => Promise.resolve({}) });
+//       });
+//       return expectSaga(fetchTodoSaga).put(fetchTodoError()).run();
+//     });
+//   });
+// });
